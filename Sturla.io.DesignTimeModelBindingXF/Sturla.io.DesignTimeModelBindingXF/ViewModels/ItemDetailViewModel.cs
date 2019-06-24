39e513ff97dd5d4ad5a29d5d82f9b96233990ec2
Sturla Thorvaldsson <sturla76@gmail.com>
2019-06-24 10:20:42 +0000

    Add project files.

---
+++ Sturla.io.DesignTimeModelBindingXF/Sturla.io.DesignTimeModelBindingXF/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Sturla.io.DesignTimeModelBindingXF.Models;
+
+namespace Sturla.io.DesignTimeModelBindingXF.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
